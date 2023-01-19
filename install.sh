8505d0a832c93f7b6563f637c26728f263737c45
Renato Palavecino <66121227+ingeniebrio77@users.noreply.github.com>
2023-01-19 14:05:30 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -92,14 +92,14 @@ echo 'LANG=de_DE.UTF-8' >>/mnt/etc/locale.conf
 # Tastatur Layout
 # echo 'KEYMAP=en' >>/mnt/etc/vconsole.conf
 echo 'KEYMAP=de' >>/mnt/etc/vconsole.conf
-cat >/mnt/etc/X11/xorg.conf.d/00-keyboard.conf <<EOF
-Section "InputClass"
-        Identifier "system-keyboard"
-        MatchIsKeyboard "on"
-        Option "XkbLayout" "de"
-        # Option "XkbLayout" "en"
-EndSection
-EOF
+#cat >/mnt/etc/X11/xorg.conf.d/00-keyboard.conf <<EOF
+#Section "InputClass"
+#        Identifier "system-keyboard"
+#        MatchIsKeyboard "on"
+#        Option "XkbLayout" "de"
+#       # Option "XkbLayout" "en"
+#EndSection
+#EOF
 
 # Hostname
 echo 'archlinux' >>/mnt/etc/hostname
